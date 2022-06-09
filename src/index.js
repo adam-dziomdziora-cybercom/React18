c29123df4c430729737b66155531c2756fef18bb
CYBERDOM2\addzi1 <adam.dziomdziora@cybercom.com>
2022-06-09 08:01:37 +0200

    index.html: added root div package.json: added babel inxed.js hello-world

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const HelloWorld = () => {
+  return <h1 className="header">Hello World!</h1>;
+};
+
+ReactDOM.render(<HelloWorld />, document.getElementById("root"));

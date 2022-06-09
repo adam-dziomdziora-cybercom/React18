468005674a444494ffa4901d31a17e9bdfd6d4e9
CYBERDOM2\addzi1 <adam.dziomdziora@cybercom.com>
2022-06-09 08:04:50 +0200

    webpack.config.js: added package.json: added path

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  devServer: {
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+};

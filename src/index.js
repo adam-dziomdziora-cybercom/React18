5aef893c67b6fb60d9afa69d40feef3d43f54136
CYBERDOM2\addzi1 <adam.dziomdziora@cybercom.com>
2022-06-15 08:17:20 +0200

    react create playground

--- src/index.js
+++ src/index.js
@@ -11,5 +11,45 @@ reactRoot.render(
     <HelloWorld myClassName="blue-header" />
     <HelloWorld myClassName="yellow-transparent-header" />
     <HelloName name="adam" myClassName="blue-header" />
+    {React.createElement(
+      'span',
+      {
+        className: 'header',
+        style: {
+          fontSize: 'xx-large',
+          fontStyle: 'oblique',
+          color: 'yellow',
+          fontVariant: 'small-caps',
+        },
+      },
+      'Hi there I love CSS!'
+    )}
+    {React.createElement(
+      HelloName,
+      { myClassName: 'yellow-transparent-header', name: 'John' },
+      null
+    )}
+    {React.createElement(
+      'div',
+      { id: 'mySuperDiv' },
+      <HelloName myClassName="yellow-transparent-header" name="Margareth" />
+    )}
+    {React.createElement(
+      'div',
+      { style: { border: '1rem dashed violet' } },
+      React.createElement(
+        'span',
+        {
+          className: 'blue-header',
+          style: {
+            fontSize: 'xx-large',
+            fontStyle: 'oblique',
+            color: 'red',
+            fontVariant: 'small-caps',
+          },
+        },
+        'Span in Div 1'
+      )
+    )}
   </React.Fragment>
 );

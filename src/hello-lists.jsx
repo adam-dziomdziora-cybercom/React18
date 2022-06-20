f7f29a0ea0274a9d6507920a44ec70808af59357
CYBERDOM2\addzi1 <adam.dziomdziora@cybercom.com>
2022-06-20 12:43:38 +0200

    jsx in variable

---
+++ src/hello-lists.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+const generateRandomIntegerInRange = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+export const HelloLists = () => {
+  const myId = 'myId';
+  const minValue = 150;
+  const maxValue = 510;
+  const listItems = [
+    generateRandomIntegerInRange(minValue, maxValue),
+    generateRandomIntegerInRange(minValue, maxValue),
+    generateRandomIntegerInRange(minValue, maxValue),
+  ].map((singleValue) => (
+    <li
+      key={singleValue}
+      className="list-group-item"
+    >{`tutaj jest string polaczony ze zmienna ${singleValue}`}</li>
+  ));
+
+  const myList = (
+    <ul id={myId} className="list-group">
+      {listItems}
+    </ul>
+  );
+
+  return (
+    <React.Fragment>
+      {React.createElement(
+        'h3',
+        { id: 'mojeh2', className: 'header' },
+        'Playing with react is cool'
+      )}
+      {React.createElement(
+        'ul',
+        { id: 'fajnalistaReactowa' },
+
+        React.createElement(
+          'li',
+          { id: 'item1', className: 'blue-header' },
+          'item 1 \u26CF;'
+        ),
+        <li id="item2" className="header">
+          item 2 hi &#x25ee;
+        </li>,
+        React.createElement(
+          'li',
+          { id: 'item3', className: 'yellow-transparent-header' },
+          'item 3 \u26CD;'
+        )
+      )}
+      {myList}
+    </React.Fragment>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HelloLists } from './hello-lists.jsx';
 
 import { HelloStuff } from './hello-stuff.jsx';
 
@@ -10,28 +11,6 @@ const reactRoot = ReactDOM.createRoot(htmlElement);
 reactRoot.render(
   <>
     <HelloStuff />
-    {React.createElement(
-      'h3',
-      { id: 'mojeh2', className: 'header' },
-      'Playing with react is cool'
-    )}
-    {React.createElement(
-      'ul',
-      { id: 'fajnalistaReactowa' },
-
-      React.createElement(
-        'li',
-        { id: 'item1', className: 'blue-header' },
-        'item 1 \u26CF;'
-      ),
-      <li id="item2" className="header">
-        item 2 hi &#x25ee;
-      </li>,
-      React.createElement(
-        'li',
-        { id: 'item3', className: 'yellow-transparent-header' },
-        'item 3 \u26CD;'
-      )
-    )}
+    <HelloLists />
   </>
 );

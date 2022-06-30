6c1b5c27d514449effb08e78d5073ee76481f236
CYBERDOM2\addzi1 <adam.dziomdziora@cybercom.com>
2022-06-30 14:56:25 +0200

    move upper

--- src/list-with-input.jsx
+++ src/list-with-input.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+const MyUL = styled.ul`
+  background: palevioletred;
+  border-radius: 3rem;
+  border: 1rem solid powderblue;
+  margin: 0 1rem;
+  padding: 0.25rem 1rem;
+  display: flex;
+`;
+
 export const ListWithInput = (props) => {
-  const MyUL = styled.ul`
-    background: palevioletred;
-    border-radius: 3rem;
-    border: 1rem solid powderblue;
-    margin: 0 1rem;
-    padding: 0.25rem 1rem;
-    display: flex;
-  `;
   return (
     <MyUL id={props.id}>
       {props.listItem}

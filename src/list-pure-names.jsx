98b7048134daf8601d1f16257b796442d12552dd
CYBERDOM2\addzi1 <adam.dziomdziora@cybercom.com>
2022-07-12 09:33:17 +0200

    list with names done

--- src/list-pure-names.jsx
+++ src/list-pure-names.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { generateRandomIntegerInRange } from './hello-lists';
+import { Button, Col, ListGroup, ListGroupItem, Row } from 'react-bootstrap';
 import maleNames from './data/names-male.json';
 import femaleNames from './data/names-female.json';
 import './style/list-pure.scss';
@@ -21,11 +22,37 @@ class ListPureNames extends React.PureComponent {
   }
 
   doStuff = () => {
-    // write code here
+    const {
+      myNames: myNamesFromState,
+      maleNamesList,
+      femaleNamesList,
+    } = this.state;
+    const randomIndex = generateRandomIntegerInRange(
+      0,
+      maleNamesList.length - 1
+    );
+    const isMale = generateRandomIntegerInRange(0, 1) === 0;
+    const randomName = isMale
+      ? maleNamesList[randomIndex]
+      : femaleNamesList[randomIndex];
+    const newNameInList = { sex: isMale ? 'man' : 'woman', name: randomName };
+    const myNames = [...myNamesFromState, newNameInList];
+    this.setState({ myNames });
   };
 
   renderNames = () => {
-    // write code here
+    const { myNames } = this.state;
+    const jsx = myNames.map((nameObj, idx) => (
+      <ListGroupItem key={idx}>
+        <Row className="centered-row">
+          <Col className={nameObj.sex === 'man' ? 'text-man' : 'text-woman'}>
+            {nameObj.sex}
+          </Col>
+          <Col className="text-name">{nameObj.name}</Col>
+        </Row>
+      </ListGroupItem>
+    ));
+    return <ListGroup>{jsx}</ListGroup>;
   };
 
   render() {
